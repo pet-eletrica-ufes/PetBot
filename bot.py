ea59fc9b3649c9bd66d84e1c4643ae3562f7bd65
OrgulhoPK <116594916+AntonioSTO@users.noreply.github.com>
2023-04-06 17:31:48 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,4 +59,4 @@ async def atualizarFila(ctx):
             filaString += f'- {user.name}\n'
     await ctx.send(filaString)
 
-bot.run('MTA5Mjk2NTYwNDY3NzM5ODU0OA.GMkNfp.67JxI2M1reZZHO5K_FlTPwwAtc0kIdkLuzLBGM')
+bot.run('TOKEN')
